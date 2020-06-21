651686d269769993440e560cd2848b550c7c93de
yijwang <yijwang@ap.xom.com>
2020-06-21 23:25:36 +0800

    [yijwang] implement for get result 0A1B for 1 number match situation

--- src/main/java/com/tw/guessnumber/ResultGenerator.java
+++ src/main/java/com/tw/guessnumber/ResultGenerator.java
@@ -2,7 +2,13 @@ package com.tw.guessnumber;
 
 public class ResultGenerator {
 
-  public String getResult(String input, String answer){
-    return "0A0B";
+  public String getResult(String input, String answer) {
+    int numberMatch = 0;
+    for (int index = 0; index < input.length(); index++) {
+      if (answer.contains(String.valueOf(input.charAt(index)))) {
+        numberMatch++;
+      }
+    }
+    return "0A"+ numberMatch +"B";
   }
 }
--- src/test/java/com/tw/guessnumber/ResultGeneratorTest.java
+++ src/test/java/com/tw/guessnumber/ResultGeneratorTest.java
@@ -12,8 +12,17 @@ public class ResultGeneratorTest {
   public void shouldReturn0A0BWhenNoNumberPositionMatchAndNoNumberMatch() {
     ResultGenerator resultGenerator = new ResultGenerator();
     assertEquals("0A0B", resultGenerator.getResult("1239", answer));
-    assertEquals("0A0B", resultGenerator.getResult("3869", answer));
+    assertEquals("0A0B", resultGenerator.getResult("3809", answer));
     assertEquals("0A0B", resultGenerator.getResult("0821", answer));
     assertEquals("0A0B", resultGenerator.getResult("8093", answer));
+  }
+
+  @Test
+  public void shouldReturn0A1BWhenNoNumberPositionMatchAnd1NumberMatch() {
+    ResultGenerator resultGenerator = new ResultGenerator();
+    assertEquals("0A1B", resultGenerator.getResult("7239", answer));
+    assertEquals("0A1B", resultGenerator.getResult("6239", answer));
+    assertEquals("0A1B", resultGenerator.getResult("4239", answer));
+    assertEquals("0A1B", resultGenerator.getResult("0539", answer));
   }
 }
