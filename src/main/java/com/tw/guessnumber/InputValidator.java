87b834f5917a49bd2641e8704175f4fe7521d6c3
yijwang <yijwang@ap.xom.com>
2020-06-22 17:25:29 +0800

    [yijwang] refactor input validator

--- src/main/java/com/tw/guessnumber/InputValidator.java
+++ src/main/java/com/tw/guessnumber/InputValidator.java
@@ -6,17 +6,23 @@ import java.util.Set;
 public class InputValidator {
 
   public boolean isValid(String input) {
+    return isNumber(input) && !isDuplication(input) && is4Digit(input);
+  }
+
+  private boolean isNumber(String input){
     String numberRegex = "\\d+";
-    if (!input.matches(numberRegex)) {
-      return false;
-    }
+    return input.matches(numberRegex);
+  }
+
+  private boolean isDuplication(String input){
     Set<Character> characterSet = new HashSet<>();
     for (int index = 0; index < input.length(); index++) {
       characterSet.add(input.charAt(index));
     }
-    if (characterSet.size() < 4) {
-      return false;
-    }
+    return characterSet.size() < 4;
+  }
+
+  private boolean is4Digit(String input){
     return input.length() == 4;
   }
 
